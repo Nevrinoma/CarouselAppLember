1d211ab2f9e0da6a9188505875a3a611b86dc157
GIULIANO MARCO <giuliano.lember@gmail.com>
2023-05-23 00:21:59 +0300

    Add project files.

---
+++ CarouselAppLember/CarouselAppLember/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CarouselApp
+{
+    public partial class MainPage : CarouselPage
+    {
+        public MainPage()
+        {
+
+            string[] colors = { "Red", "Blue", "Green", "Yellow" };
+
+            for (int i = 0; i < colors.Length; i++)
+            {
+                var contentPage = new ContentPage
+                {
+                    Content = new StackLayout
+                    {
+                        Children =
+                        {
+                            new Label
+                            {
+                                Text= colors[i],
+                                FontSize= Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                                HorizontalOptions= LayoutOptions.Center
+                            },
+                            new BoxView
+                            {
+                                Color= Color.FromHex(colors[i]),
+                                WidthRequest= 200,
+                                HeightRequest= 200,
+                                HorizontalOptions= LayoutOptions.Center,
+                                VerticalOptions= LayoutOptions.CenterAndExpand
+                            }
+                        }
+                    }
+                };
+
+                Children.Add(contentPage);
+            }
+        }
+    }
+}
