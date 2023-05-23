4a5b4386b63492599d85dcdfd2462c95632af605
opilane <opilane@HP-CZC2349HT0>
2023-05-23 15:55:29 +0300

    dsd

--- CarouselAppLember/CarouselAppLember/ColorsPages.cs
+++ CarouselAppLember/CarouselAppLember/ColorsPages.cs
@@ -9,54 +9,88 @@ namespace CarouselAppLember
 {
     public class ColorsPages : CarouselPage
     {
+        private Dictionary<string, Color> colorDictionary;
+        private StackLayout colorsLayout;
+
         public ColorsPages()
         {
-            var colorDictionary = new Dictionary<string, Color>
+            colorDictionary = new Dictionary<string, Color>
+        {
+            { "Red", Color.Red },
+            { "Blue", Color.Blue },
+            { "Green", Color.Green },
+            { "Yellow", Color.Yellow }
+        };
+
+            colorsLayout = new StackLayout();
+
+            RefreshColors(contentView);
+
+            Children.Add(new ContentPage
             {
-                { "Red", Color.Red},
-                { "Blue", Color.Blue},
-                { "Green", Color.Green},
-                { "Yellow", Color.Yellow}
-            };
+                Content = colorsLayout
+            });
+        }
+
+        public void AddColor(string colorName)
+        {
+            if (!colorDictionary.ContainsKey(colorName))
+            {
+                colorDictionary.Add(colorName, GetRandomColor());
+                RefreshColors(contentView);
+            }
+        }
+
+        private void RefreshColors(ContentView contentView)
+        {
+            colorsLayout.Children.Clear();
 
             foreach (var color in colorDictionary)
             {
-                var contentPage = new ContentPage
+                var contentView = new ContentView
                 {
                     Content = new StackLayout
                     {
                         Children =
+                    {
+                        new Label
                         {
-                            new Label
+                            Text= color.Key,
+                            FontSize= Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                            HorizontalOptions= LayoutOptions.Center
+                        },
+                        new BoxView
+                        {
+                            Color= color.Value,
+                            WidthRequest= 200,
+                            HeightRequest= 200,
+                            HorizontalOptions= LayoutOptions.Center,
+                            VerticalOptions= LayoutOptions.CenterAndExpand
+                        },
+                        new Button
+                        {
+                            Text = "Tagasi",
+                            HorizontalOptions = LayoutOptions.Center,
+                            VerticalOptions = LayoutOptions.End,
+                            Command = new Command(async () =>
                             {
-                                Text= color.Key,
-                                FontSize= Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
-                                HorizontalOptions= LayoutOptions.Center
-                            },
-                            new BoxView
-                            {
-                                Color= color.Value,
-                                WidthRequest= 200,
-                                HeightRequest= 200,
-                                HorizontalOptions= LayoutOptions.Center,
-                                VerticalOptions= LayoutOptions.CenterAndExpand
-                            },
-                            new Button
-                            {
-                                Text = "Tagasi",
-                                HorizontalOptions = LayoutOptions.Center,
-                                VerticalOptions = LayoutOptions.End,
-                                Command = new Command(async () =>
-                                {
-                                    await Navigation.PushModalAsync(new MainPage());
-                                })
-                            }
+                                await Navigation.PushModalAsync(new MainPage());
+                            })
                         }
+                    }
                     }
                 };
 
-                Children.Add(contentPage);
+                Children.Add(contentView);
             }
         }
+
+        private Color GetRandomColor()
+        {
+            Random random = new Random();
+            return Color.FromRgb(random.Next(256), random.Next(256), random.Next(256));
+        }
     }
+
+
 }
--- CarouselAppLember/CarouselAppLember/MainPage.cs
+++ CarouselAppLember/CarouselAppLember/MainPage.cs
@@ -9,8 +9,11 @@ namespace CarouselAppLember
 {
     public partial class MainPage : ContentPage
     {
-        List<CarouselPage> ContentPages = new List<CarouselPage>() { new ColorsPages()};
+        List<CarouselPage> ContentPages = new List<CarouselPage>() { new ColorsPages() };
         List<string> tekstid = new List<string>() { "Colors" };
+        Entry colorEntry;
+        ColorsPages colorsPages;
+
         public MainPage()
         {
             Title = "Menu";
@@ -18,10 +21,27 @@ namespace CarouselAppLember
             StackLayout st1 = new StackLayout()
             {
                 Orientation = StackOrientation.Vertical,
-                VerticalOptions= LayoutOptions.Center,
-                HorizontalOptions= LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.Center,
                 BackgroundColor = Color.White,
             };
+
+            colorEntry = new Entry()
+            {
+                Placeholder = "Enter color name"
+            };
+
+            Button addButton = new Button()
+            {
+                Text = "Add",
+                BackgroundColor = Color.Gray,
+                TextColor = Color.Black
+            };
+            addButton.Clicked += AddButton_Clicked;
+
+            st1.Children.Add(colorEntry);
+            st1.Children.Add(addButton);
+
             for (int i = 0; i < ContentPages.Count; i++)
             {
                 Button button = new Button()
@@ -36,12 +56,24 @@ namespace CarouselAppLember
             }
             Content = st1;
 
-
+            colorsPages = (ColorsPages)ContentPages[0];
         }
+
+        private void AddButton_Clicked(object sender, EventArgs e)
+        {
+            string colorName = colorEntry.Text;
+            if (!string.IsNullOrEmpty(colorName))
+            {
+                colorsPages.AddColor(colorName);
+                colorEntry.Text = string.Empty;
+            }
+        }
+
         private async void Navig_funktsion(object sender, EventArgs e)
         {
             Button b = (Button)sender;
             await Navigation.PushModalAsync(ContentPages[b.TabIndex]);
         }
     }
+
 }
